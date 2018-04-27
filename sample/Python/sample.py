5615913cb00c491d4b92d5108b05082e89f88f08
Marissa <35174396+mUtterberg@users.noreply.github.com>
2018-04-27 06:29:06 -0400

    Update sample.py
    Pasted Career Change phase 1 XML iteration as sample Python script for code snippet presentation methods. Replace with Beautiful Soup or keep to compare?

--- sample/Python/sample.py
+++ sample/Python/sample.py
@@ -1,3 +1,41 @@
-# sample
-# Comment out in Atom with command /
-# Only works after saved as recognized suffix - needs to know syntax!
+# Sample Python script for code snippet presentation methods
+
+# -*- coding: utf-8 -*-
+
+# Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+# Importing the dataset
+tree = ET.parse('xml-compilation.xml')
+branches = tree.
+root = tree.getroot()
+
+# Exploring root information
+root.tag
+root.attrib
+
+data = []
+for neighbor in root.findall("occupation"):
+    name = neighbor.find("title")
+    nameStr = neighbor.find("title").text
+    print(nameStr)
+    data.append(nameStr)
+ 
+
+hierarchy = []
+for neighbor in tree.iter():
+    name = neighbor.tag
+    hierarchy.append(name)
+print(hierarchy)
+
+
+frame = pd.DataFrame(data)
+frame.to_csv("occupations.csv")
+
+hFrame = pd.DataFrame(hierarchy)
+hFrame.to_csv("contents.csv")
